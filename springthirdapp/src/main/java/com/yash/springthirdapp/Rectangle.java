e61b96407e334e28b0a9945f45f376e653d27cc6
Jaie93 <69464507+Jaie93@users.noreply.github.com>
2022-09-15 11:00:53 +0530

    Add files via upload
    Spring IOC assignment for abstract class

---
+++ springthirdapp/src/main/java/com/yash/springthirdapp/Rectangle.java
@@ -0,0 +1,11 @@
+package com.yash.springthirdapp;
+
+public class Rectangle extends Shape {
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		System.out.println("Drawing rectangle");
+	}
+
+}
